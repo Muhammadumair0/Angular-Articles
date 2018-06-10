161754c9ac799920f15a21d4f1c6802cdb083a8b
Muhammadumair0 <muhammadumairkhanyz@gmail.com>
2018-06-10 16:36:43 +0500

    m-login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { routes } from './login-routing.module';
 import { LoginComponent } from "./login.component";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [LoginComponent],
---
+++ src/server/test-user/seed-db.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const users = require("./test-user.json");
+
+const MongoClient = require("mongodb").MongoClient;
+const bcrypt = require("bcrypt");
+const path = require("path");
+
+function seedCollection(collectionName, initialRecords) {
+
+    MongoClient.connect(process.env.DB_CONN, (err, database) => {
+        console.log("Connected to mongoDB driver!!", process.env.DB_CONN);
+        if (err) {
+
+            console.log("Here is the error", err);
+
+        } else {
+            const myDB = database.db('ld-readit')
+            const db = myDB.collection(collectionName);
+
+            db.remove();
+
+            initialRecords.password = bcrypt.hashSync(initialRecords.password, 10);
+
+
+            db.insert(initialRecords, (err, result) => {
+
+                console.log(`Number of data inserted:${result.insertedCount}`);
+                console.log("closing connection!!");
+                database.close();
+            });
+        }
+    });
+}
+
+
+seedCollection('users', users);
